1aa3db94787c4f68162b404d7643b4bd29024096
Lily Ruddy <lruddy@smith.edu>
2024-09-30 16:43:45 -0400

    started on engine

--- Engine.java
+++ Engine.java
@@ -1,3 +1,45 @@
 public class Engine {
 
+    // Attributes:
+    String FeulType; // indicates which fuel it uses 
+    private double currentFuel; // current fuel level
+    private double maxFuel; // max fuel level
+
+    /**
+     * Constructor
+     * @param Fuel type
+     * @param Current fuel level
+     * @param Max fuel level
+     */
+    public Engine(String FeulType, double currentFuel, double maxFuel){
+        this.FeulType = FeulType;
+        this.currentFuel = currentFuel;
+        this.maxFuel = maxFuel;
+    }
+
+    // Getters:
+
+    /* Fuel type getter */
+    public String getFuelType(){
+        return this.FeulType;
+    }
+    /* Current fuel getter */
+    public double getcurrentFuel(){
+        return this.currentFuel;
+    }
+    /* Max fuel getter */
+    public double getmaxFuel(){
+        return  this.maxFuel;
+    }
+
+
+
+
+    // Main:
+    public static void main(String[] args) {
+        Engine myEngine = new Engine("coal", 2, 10);
+        System.out.println("My engine has fuel type of " + myEngine.FeulType);
+    }
+
+
 }
