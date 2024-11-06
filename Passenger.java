8c41e1876a7540c1aa1ae76557d439cfbc205ba7
Lily Ruddy <lruddy@smith.edu>
2024-11-06 10:55:52 -0500

    passenger fixed

--- Passenger.java
+++ Passenger.java
@@ -4,37 +4,59 @@ public class Passenger {
     
     // Attributes:
     private String name; // passenger's name
-    // private ArrayList<String> Cars = new ArrayList<String>(); 
+    private Car c; // car that the passenger is in
+    
 
     // Constructor:
+    /**
+     * Creates a Passenger class.
+     * @param String name; name of passenger
+     */
     public Passenger(String name) {
         this.name = name;
     }
 
     // Getters
-    /* Name getter */
+    /**
+     * Getter for the passenger's name.
+     * @return String name; name of passenger
+     */
     public String getname(){
         return this.name;
     }
 
+    /**
+     * Getter for the Car class. 
+     * @return Car c; car that the passenger is in
+     */
+    public Car getcar(){
+        return this.c;
+    }
+
     // Methods:
-    // public void boadCard(ArrayList<String> Car_c){
-    //     /* Checks if the selected car is full */
-    //     if(Car_c.seatsRemaining >= 1){
+    public void boardCar(Car c){
+        /* Check if the selected car is full */
+        if(c.addPassenger(this.name) == false){
+            System.out.println(this.c + " is at max capacity.");
 
-
-
-
-    //     } else{
-
-    //     }
-
-    // }
+        } else if(c.addPassenger(this.name) == true){
+            System.out.println(this.name +" boarded " + this.c);
+            
+        } else{
+            throw new RuntimeException("Please try again.");
+        }
+    }
+   
 
 
 
     // Main:
     public static void main(String[] args) {
+        Car newCar = new Car(null, 1);
+        Passenger myPassenger = new Passenger("Theo");
         
+        /* Boarding passenger */
+        myPassenger.boardCar(newCar);
+
     }
 }
